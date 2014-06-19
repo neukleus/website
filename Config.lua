8d2510b7c2f315a5b6f6cd6f24479051b7ca4f6c
milind <milind.gupta@gmail.com>
2014-06-19 16:46:52 -0700

    Fixed the Google +1 button

--- Config.lua
+++ Config.lua
@@ -142,7 +142,7 @@ LTMAP = {
 		<a href="https://twitter.com/share" class="twitter-share-button" data-hashtags="Neukleus">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 <!-- Place this tag where you want the +1 button to render. -->
-<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="300"></div>
+<div class="g-plusone" data-size="medium"></div>
 
 <script src="//platform.linkedin.com/in.js" type="text/javascript">
   lang: en_US
@@ -152,7 +152,7 @@ LTMAP = {
 						<a href="https://twitter.com/share" class="twitter-share-button" data-hashtags="Neukleus">Tweet</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 				<!-- Place this tag where you want the +1 button to render. -->
-				<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="300"></div>
+				<div class="g-plusone" data-size="medium"></div>
 
 <!-- Place this tag after the last +1 button tag. -->
 <script type="text/javascript">
